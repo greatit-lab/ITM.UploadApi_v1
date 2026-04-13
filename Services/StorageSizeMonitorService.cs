0a3d91b710459fc36dc1b7e7d3f47d9ee41fe9ea
greatit-lab <gil2zum@gmail.com>
2026-04-13 19:07:03 +0900

    Update StorageSizeMonitorService.cs --0413_19-1

--- Services/StorageSizeMonitorService.cs
+++ Services/StorageSizeMonitorService.cs
@@ -11,7 +11,7 @@ namespace ITM.UploadApi.Services
     public class StorageSizeMonitorService : BackgroundService
     {
         private readonly string _baseStoragePath;
-        
+
         // 다른 클래스(Controller)에서 즉시 값을 가져갈 수 있도록 static 선언
         public static long CachedSizeBytes { get; private set; } = 0;
 
@@ -31,13 +31,13 @@ namespace ITM.UploadApi.Services
                     {
                         long totalSize = 0;
                         var dirInfo = new DirectoryInfo(_baseStoragePath);
-                        
+
                         // 하위의 모든 파일을 스캔하여 용량 합산 (백그라운드에서 실행되므로 API 렉 유발 없음)
                         foreach (var file in dirInfo.EnumerateFiles("*", SearchOption.AllDirectories))
                         {
                             totalSize += file.Length;
                         }
-                        
+
                         // 계산이 완료되면 전역 변수 업데이트
                         CachedSizeBytes = totalSize;
                     }
